4c6543ef4f0490e002cc952260584d2a6eec94a7
BT <bernardotolosajr@gmail.com>
2016-07-25 11:38:40 +0800

    Initial Commit

---
+++ cpr-example/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cpr-example
+//
+//  Created by Bernard Toloza on 25/7/16.
+//  Copyright © 2016 GXI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ cpr-example/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cpr-example
+//
+//  Created by Bernard Toloza on 25/7/16.
+//  Copyright © 2016 GXI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
